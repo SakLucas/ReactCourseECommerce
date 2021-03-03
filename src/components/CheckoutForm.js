f9ade7680a77abe29721ee588ab9ef6edb0bca8d
ThisIsNotAJoke <lucas.n.sak@gmail.com>
2021-03-03 00:09:06 -0300

    fix button Realizar pedido disabling

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -48,7 +48,7 @@ const CheckoutForm = ({cart, subtotal}) => {
             <TextField type="email" label="mail"  name="mail" id="" placeholder="user@mail.com" value={form.mail} onChange={e => handleForm(e)} />
             <TextField type="email" label="mail"  name="mailValid" id="" placeholder="user@mail.com" value={form.mailValid} onChange={e => handleForm(e)} />
             <div style={{marginBottom:'15px'}}>{error.length >= 1 && <span style={{color:'red'}}>{error}</span>}</div>
-            <Button variant="contained" color="secondary" onClick={() => handleSubmit(cart, subtotal, form)} disabled={form.name.length < 2 && form.tel.length < 8 && form.mail.length < 6 && form.mail !== form.mailValid}>Realizar pedido</Button>
+            <Button variant="contained" color="secondary" onClick={() => handleSubmit(cart, subtotal, form)} disabled={Object.keys(order).length === 2 || form.name.length < 2 && form.tel.length < 8 && form.mail.length < 6 && form.mail !== form.mailValid}>Realizar pedido</Button>
             {Object.keys(order).length === 2 && <p>Gracias por tu compra, {order.name}! Tu número de orden es {order.id}</p>}
         </div>
     )
